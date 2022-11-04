f7b03cd508effbd98dde5c6af35a29975676773d
fibr <fabry.d@seznam.cz>
2022-11-04 21:32:20 +0100

    obsahlejsi homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,49 @@
 import React from 'react'
-import { Link, Typography } from '@mui/material'
+import { Box, Link, Typography } from '@mui/material'
 import { AppPageWrapper } from '../components/common/AppPageWrapper'
+import { useDialog } from '../hooks/useDialog'
+import { DialogType } from '../types/common'
 
 export function Home() {
+	const { showDialog } = useDialog()
 	return (
 		<AppPageWrapper>
-			<img
-				src="https://media.api-sports.io/football/leagues/1.png"
-				alt="FIFA WORLD CUP 2022"
-			/>
+			<Box sx={{ padding: '20px' }}>
+				<img
+					src="https://media.api-sports.io/football/leagues/1.png"
+					alt="FIFA WORLD CUP 2022"
+				/>
+			</Box>
 			<Typography variant="h4" gutterBottom>
+				Tipovací soutěž
+			</Typography>
+			<Typography sx={{ textAlign: 'center', padding: '10px' }} variant="h6">
+				Pro více podrobností k soutěži se prosím{' '}
 				<Link
+					sx={{ cursor: 'pointer', textDecoration: 'none' }}
+					onClick={() => showDialog(DialogType.LOGIN)}
+				>
+					přihlašte
+				</Link>
+				, případně si{' '}
+				<Link
+					sx={{ cursor: 'pointer', textDecoration: 'none' }}
+					onClick={() => showDialog(DialogType.REGISTRATION)}
+				>
+					zaregistrujte
+				</Link>{' '}
+				nový účet.
+			</Typography>
+			<Typography sx={{ textAlign: 'center', padding: '10px' }}>
+				Oficiální stránky mistrovství naleznete{' '}
+				<Link
+					sx={{ cursor: 'pointer', textDecoration: 'none' }}
 					href="https://www.fifa.com/fifaplus/en/tournaments/mens/worldcup/qatar2022"
 					target="_blank"
 				>
-					FIFA WORLD CUP 2022
+					zde
 				</Link>
+				.
 			</Typography>
 		</AppPageWrapper>
 	)
