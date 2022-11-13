f843944984bb2a75ce214fe8af244b20d9effcc7
fibr <fabry.d@seznam.cz>
2022-11-13 10:42:19 +0100

    sjednoceni vykresleni skupin

--- src/components/GroupRow.tsx
+++ src/components/GroupRow.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Avatar, Stack, Paper, Typography } from '@mui/material'
+import { Avatar, Stack, Paper, Typography, Box } from '@mui/material'
 import { Standing } from '../types/standings'
 
 interface GroupRowProps {
@@ -18,49 +18,53 @@ export function GroupRow({ standings }: GroupRowProps) {
 	const items = standings.map(createData)
 
 	return (
-		<Stack
-			direction={{ xs: 'row', sm: 'row' }}
-			spacing={{ xs: 0, md: 1 }}
+		<Box
 			margin={{ xs: 1, md: 2 }}
+			padding={{ xs: 1, md: 1 }}
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				bgcolor: 'background.paper',
+				boxShadow:
+					'0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)',
+			}}
 		>
-			{items.map(({ teamName, logo, group }) => (
-				<Paper
-					sx={{
-						display: 'flex',
-						flexDirection: 'column',
-						alignItems: 'center',
-						width: '24vw',
-						maxWidth: '200px',
-						padding: '1vw',
-					}}
-					key={teamName}
-				>
-					<Typography
-						variant="h6"
-						fontSize={{ xs: '1rem', md: '1.2rem' }}
-						sx={{ textAlign: 'center', whiteSpace: 'nowrap' }}
-					>
-						{group}
-					</Typography>
-					<Typography
-						variant="h6"
-						fontSize={{ xs: '0.8rem', md: '1rem' }}
-						sx={{ textAlign: 'center' }}
-					>
-						{teamName}
-					</Typography>
-					<Avatar
+			<Typography variant="h6">{items[0].group}</Typography>
+			<Stack direction={{ xs: 'row', sm: 'row' }} spacing={{ xs: 0, md: 1 }}>
+				{items.map(({ teamName, logo }) => (
+					<Paper
 						sx={{
-							width: '60px',
-							height: '40px',
-							margin: '5px',
-							outline: '1px solid grey',
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'center',
+							width: '24vw',
+							maxWidth: '200px',
+							padding: '1vw',
+							boxShadow: 'none',
 						}}
-						src={logo}
-						variant="square"
-					/>
-				</Paper>
-			))}
-		</Stack>
+						key={teamName}
+					>
+						<Typography
+							variant="h6"
+							fontSize={{ xs: '0.8rem', md: '1rem' }}
+							sx={{ textAlign: 'center' }}
+						>
+							{teamName}
+						</Typography>
+						<Avatar
+							sx={{
+								width: '60px',
+								height: '40px',
+								margin: '5px',
+								outline: '1px solid grey',
+							}}
+							src={logo}
+							variant="square"
+						/>
+					</Paper>
+				))}
+			</Stack>
+		</Box>
 	)
 }
